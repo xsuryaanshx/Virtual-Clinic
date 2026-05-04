fccc4f85a647d7cd392038a9e96e3e94fbc1b1cc
xsuryaanshx <1923.suryansh@gmail.com>
2026-05-04 21:37:58 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css";  // ← THIS LINE WAS MISSING!
 
 // 🔥 Prevent Promise crash from breaking UI
 window.addEventListener("unhandledrejection", (event) => {
